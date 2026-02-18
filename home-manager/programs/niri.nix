7403edc108a5b0f17328038121acd4aeb7a81e61
shizumu-iruka <github@kairi6.anonaddy.com>
2026-02-18 18:04:35 +0100

    Improve niri keybinds

--- home-manager/programs/niri.nix
+++ home-manager/programs/niri.nix
@@ -40,7 +40,7 @@
 
       binds =
       let
-        noctalia-shell = args: {
+        noctalia = args: {
           action.spawn = [
             "noctalia-shell"
             "ipc"
@@ -51,49 +51,40 @@
         sh = cmd: { action.spawn-sh = cmd; };
       in
       {
-        "Mod+S" = noctalia-shell [
+        "Mod+S" = noctalia [
           "controlCenter"
           "toggle"
         ];
-        "Mod+Comma" = noctalia-shell [
+        "Mod+Comma" = noctalia [
           "settings"
           "toggle"
         ];
-        "Mod+N" = noctalia-shell [
-          "notifications"
-          "toggle"
-        ];
-        "Mod+Space" = noctalia-shell [
+        "Mod+Space" = noctalia [
           "launcher"
           "toggle"
         ];
-        "Mod+V" = noctalia-shell [
-          "clipboard"
-          "toggle"
-        ];
-        "XF86AudioLowerVolume" = noctalia-shell [
+        "XF86AudioLowerVolume" = noctalia [
           "volume"
           "decreease"
        ];
-        "XF86AudioRaiseVolume" = noctalia-shell [
+        "XF86AudioRaiseVolume" = noctalia [
           "volume"
 	  "increase"
 	];
-        "XF86AudioMute" = noctalia-shell [
+        "XF86AudioMute" = noctalia [
           "volume"
           "muteOutput"
         ];
-        "XF86MonBrightnessDown" = noctalia-shell [
+        "XF86MonBrightnessDown" = noctalia [
           "brightness"
           "decrease"
         ];
-        "XF86MonBrightnessUp" = noctalia-shell [
+        "XF86MonBrightnessUp" = noctalia [
           "brightness"
           "increase"
         ];
 
         # Niri-specific config starts here
-        "Mod+0".action.focus-workspace = 10;
         "Mod+1".action.focus-workspace = 1;
         "Mod+2".action.focus-workspace = 2;
         "Mod+3".action.focus-workspace = 3;
@@ -104,25 +95,41 @@
         "Mod+8".action.focus-workspace = 8;
         "Mod+9".action.focus-workspace = 9;
 
+        "Mod+Ctrl+1".action.move-column-to-workspace = 1;
+        "Mod+Ctrl+2".action.move-column-to-workspace = 2;
+        "Mod+Ctrl+3".action.move-column-to-workspace = 3;
+        "Mod+Ctrl+4".action.move-column-to-workspace = 4;
+        "Mod+Ctrl+5".action.move-column-to-workspace = 5;
+        "Mod+Ctrl+6".action.move-column-to-workspace = 6;
+        "Mod+Ctrl+7".action.move-column-to-workspace = 7;
+        "Mod+Ctrl+8".action.move-column-to-workspace = 8;
+        "Mod+Ctrl+9".action.move-column-to-workspace = 9;
+
         "Mod+BracketLeft".action.consume-or-expel-window-left = [ ];
         "Mod+BracketRight".action.consume-or-expel-window-right = [ ];
         "Mod+C".action.center-column = [ ];
+	"Mod+Ctrl+C".action.center-visible-columns = [ ];
         "Mod+F".action.maximize-column = [ ];
-        "Mod+H".action.focus-column-left = [ ];
-        "Mod+J".action.focus-window-or-workspace-down = [ ];
-        "Mod+K".action.focus-window-or-workspace-up = [ ];
-        "Mod+L".action.focus-column-right = [ ];
-	"Mod+Tab".action.toggle-overview = [ ];
+	"Mod+M".action.maximize-window-to-edges = [ ];
 
-	  # Fallback keybind for non-vim users
+        "Mod+K".action.focus-window-or-workspace-up = [ ];
+        "Mod+J".action.focus-window-or-workspace-down = [ ];
+        "Mod+H".action.focus-column-left = [ ];
+        "Mod+L".action.focus-column-right = [ ];
+
+	"Mod+Up".action.focus-window-or-workspace-up = [ ];
+	"Mod+Down".action.focus-window-or-workspace-down = [ ];
 	"Mod+Left".action.focus-column-left = [ ];
 	"Mod+Right".action.focus-column-right = [ ];
 
         "Mod+R".action.switch-preset-column-width-back = [ ];
+	"Mod+Tab".action.toggle-overview = [ ];
 
         "Mod+T".action.spawn = "kitty";
 	"Mod+W".action.spawn = "vivaldi";
-	"Mod+Y" = sh "yazi";
+	"Mod+Y" = sh "kitty -e yazi";
+	"Mod+N" = sh "kitty -e nvim";
+
         "Mod+Shift+C".action.center-visible-columns = [ ];
         "Mod+Shift+F".action.fullscreen-window = [ ];
         "Mod+Shift+H".action.move-column-left = [ ];
@@ -131,8 +138,23 @@
         "Mod+Shift+L".action.move-column-right = [ ];
         "Mod+Shift+R".action.switch-preset-column-width = [ ];
 
-        "Mod+Q".action.close-window = [ ]; 
+	"Mod+Home".action.focus-column-first = [ ];
+	"Mod+End".action.focus-column-last = [ ];
+        "Mod+Ctrl+Home".action.move-column-to-first = [ ];
+        "Mod+Ctrl+End".action.move-column-to-last = [ ];
+
+	"Mod+Shift+E".action.quit = [ ];
+	"Mod+Shift+Slash".action.show-hotkey-overlay = [ ];
+
+	"Mod+V".action.toggle-window-floating = [ ];
+	"Mod+Shift+V".action.switch-focus-between-floating-and-tiling = [ ];
+
         "Print".action.screenshot = [ ];
+	"Ctrl+Print".action.screenshot-screen = [ ];
+	"Alt+Print".action.screenshot-window = [ ];
+
+        "Mod+Shift+P".action.power-off-monitors = [ ];
+	"Mod+Q".action.close-window = [ ]; 
       };
     };
   };
